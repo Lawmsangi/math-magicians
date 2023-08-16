e0f9cbaab6f7b3cfc598d2b974c10c1b58f012ee
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-08-16 15:33:48 +0300

    Update operate.test.js

--- src/__tests__/operate.test.js
+++ src/__tests__/operate.test.js
@@ -1,9 +1,8 @@
-import operate from "../logic/operate";
+import operate from '../logic/operate';
 
 describe('Operations should be as below', () => {
   it('should subtract two numbers', () => {
     const result = operate('15.5', '5.5', '-');
     expect(result).toBe('10');
   });
-
 });
